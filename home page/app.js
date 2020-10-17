b9c3db9f15c3e8ea49a60a187c2b041393933aae
Rishikesh1159 <rishireddy1159@gmail.com>
2020-10-17 19:28:22 -0400

    swe645 group project

---
+++ home page/app.js
@@ -0,0 +1,9 @@
+const people = [{ name: "reddy", age: 20, position="dev" }, { name: "chinal", age: 23, position="dev" }, { name: "bhosdk", age: 19, position="dev" }, { name: "f***", age: 25, position="dev" }]
+
+const mapex=people.map(function(person){
+ return person;
+});
+
+console.log(mapex);
+
+console.log("hii");
